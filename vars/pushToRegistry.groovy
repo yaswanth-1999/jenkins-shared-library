4bce78d743e99a489f458c8e229e6fdd9812fa17
yaswanthmike <yaswanth.kora@gmail.com>
2025-12-29 16:33:53 +0530

    added new file

--- vars/pushToRegistry.groovy
+++ vars/pushToRegistry.groovy
@@ -1,9 +1,19 @@
-// vars/pushToRegistry.groovy
 def call(String imageName, String tag = 'latest') {
 
     if (imageName.contains(':')) {
-        error "Invalid imageName '${imageName}'. Do not include tag. Pass tag separately."
+        error "Invalid imageName '${imageName}'. Do not include tag."
     }
 
-    sh "docker push ${imageName}:${tag}"
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerhub-creds',
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+
+        sh """
+            docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+            docker tag ${imageName}:${tag} ${DOCKER_USER}/${imageName}:${tag}
+            docker push ${DOCKER_USER}/${imageName}:${tag}
+        """
+    }
 }

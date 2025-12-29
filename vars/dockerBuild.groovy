cb0553e53de8fbb777bde84541f253dbfb247c4c
yaswanthmike <yaswanth.kora@gmail.com>
2025-12-29 14:39:43 +0530

    added new file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,12 @@
-def call(String imageName, String tag ) {
+// vars/dockerBuild.groovy
+def call(String imageName, String tag = 'latest') {
 
+    // Validate imageName does not contain colon
     if (imageName.contains(':')) {
         error "Invalid imageName '${imageName}'. Do not include tag. Pass tag separately."
     }
 
+    // Docker build
     sh """
         docker build -t ${imageName}:${tag} .
     """

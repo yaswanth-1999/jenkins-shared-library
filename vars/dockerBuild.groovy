30f21978c74ba1d5d1f45ffa83a4c11b030cfe0b
yaswanthmike <yaswanth.kora@gmail.com>
2025-12-29 14:27:03 +0530

    added new file

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,9 @@
 def call(String imageName, String tag = 'latest') {
+
+    if (imageName.contains(':')) {
+        error "Invalid imageName '${imageName}'. Do not include tag. Pass tag separately."
+    }
+
     sh """
         docker build -t ${imageName}:${tag} .
     """
